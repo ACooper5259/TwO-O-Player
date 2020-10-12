bd2a0fe9988c792183e7f83f026139ffb66b85f9
ACooper5259 <audrey_cooper@me.com>
2020-10-12 12:10:36 -0700

    create question class and test initialisation

---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative "question"
+
+question1 = Question.new("what is 2 + 3 ? ", 5)
+
+puts question1.text
+puts question1.answer
---
+++ question.rb
@@ -0,0 +1,10 @@
+class Question
+  attr_reader :text, :answer
+
+  def initialize(text, answer)
+    @text = text
+    @answer = answer
+  end
+
+end
+    
