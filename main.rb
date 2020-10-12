6ac3feaf9be16f60bef6b1120e35a02a3f62c6d1
ACooper5259 <audrey_cooper@me.com>
2020-10-12 12:59:11 -0700

    add player class

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require_relative "question"
+require_relative "player"
 
 question1 = Question.new("what is 2 + 3 ? ", 5)
 
 puts question1.text
 puts question1.answer
+
+player1 = Player.new
+puts player1.lives
+puts player1.score
---
+++ player.rb
@@ -0,0 +1,9 @@
+class Player
+  attr_accessor :lives, :score
+
+  def initialize()
+    self.lives = 3
+    self.score = 0
+  end
+  
+end
